87cd15ad2d4aa3ed812970af08df0f2685b098e1
FelZimmer <fozimermann@gmail.com>
2026-04-15 21:30:58 -0300

    Questão 1 – Condicional If

---
+++ script.js
@@ -0,0 +1,8 @@
+//Questão 1 – Condicional If
+let tarefaConcluida = true;
+
+if (tarefaConcluida) {
+    console.log("A tarefa está concluída!");
+} else {
+    console.log("A tarefa não foi concluída.");
+}
