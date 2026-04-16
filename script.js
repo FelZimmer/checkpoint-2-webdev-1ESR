c8c744605d85e4964c1265cdd443bcf05ac7bafb
FelZimmer <fozimermann@gmail.com>
2026-04-15 21:34:32 -0300

    Questão 2 – Condicional If/Else If/Else

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ if (tarefaConcluida) {
 } else {
     console.log("A tarefa não foi concluída.");
 }
+// Questão 2 – Condicional If/Else If/Else
+let prioridade = 2;
+
+if (prioridade === 1) {
+    console.log("Prioridade Baixa");
+
+} else if (prioridade === 2) {
+    console.log("Prioridade Média");
+
+} else if (prioridade === 3) {
+    console.log("Prioridade Alta");
+}
