beeb2f2a1293aec54fd4d1da707671ff598bc21a
FelZimmer <fozimermann@gmail.com>
2026-04-15 21:39:13 -0300

    Questão 3 – Switch Case

--- script.js
+++ script.js
@@ -18,3 +18,29 @@ if (prioridade === 1) {
 } else if (prioridade === 3) {
     console.log("Prioridade Alta");
 }
+
+//  Questão 3 – Switch Case
+let diaSemana = new Date().getDay(); 
+switch (diaSemana) {
+    case 0:
+        console.log("Domingo");
+        break;
+    case 1:
+        console.log("Segunda-feira");
+        break;
+    case 2:
+        console.log("Terça-feira");
+        break;
+    case 3:
+        console.log("Quarta-feira");
+        break;
+    case 4:
+        console.log("Quinta-feira");
+        break;
+    case 5:
+        console.log("Sexta-feira");
+        break;
+    case 6:
+        console.log("Sábado");
+        break;
+}
